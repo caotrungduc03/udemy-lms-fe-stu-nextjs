98afc3ce210b99242f01b898169bda1fe4356d8c
caotrungduc <trungdc2003@gmail.com>
2024-07-29 10:09:41 +0700

    feat: add query api login

---
+++ lib/features/auth/authApi.ts
@@ -0,0 +1,28 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const authApi = createApi({
+  reducerPath: 'authApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: process.env.NEXT_PUBLIC_API_URL + '/auth',
+  }),
+  endpoints: (builder) => ({
+    login: builder.mutation<any, { email: string; password: string }>({
+      query: (data) => ({
+        url: '/login',
+        method: 'POST',
+        body: data,
+      }),
+    }),
+    getAuthData: builder.query<any, { accessToken: string }>({
+      query: ({ accessToken }) => ({
+        url: '/me',
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }),
+    }),
+  }),
+});
+
+export const { useLoginMutation, useGetAuthDataQuery } = authApi;
---
+++ lib/features/auth/authSlice.ts
@@ -0,0 +1,46 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { setToken } from '../../tokens';
+import { authApi } from './authApi';
+
+const initialState = {
+  user: null,
+};
+
+export const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    logout: (state) => {
+      localStorage.removeItem('accessToken');
+      state.user = null;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addMatcher(
+        authApi.endpoints.login.matchFulfilled,
+        (state, { payload }) => {
+          setToken(payload.data.accessToken);
+
+          return {
+            ...state,
+            user: payload.data.user,
+          };
+        },
+      )
+      .addMatcher(
+        authApi.endpoints.getAuthData.matchFulfilled,
+        (state, { payload }) => {
+          setToken(payload.data.accessToken);
+
+          return {
+            ...state,
+            user: payload.data.user,
+          };
+        },
+      );
+  },
+});
+
+export const { logout } = authSlice.actions;
+export default authSlice.reducer;
