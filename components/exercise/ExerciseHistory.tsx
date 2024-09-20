2d9eeeaac2eca601adc67647763833f69738e81a
caotrungduc <trungdc2003@gmail.com>
2024-09-20 09:58:05 +0700

    feat: create detail submission page

--- components/exercise/ExerciseHistory.tsx
+++ components/exercise/ExerciseHistory.tsx
@@ -1,5 +1,4 @@
 import { usePathname, useRouter } from 'next/navigation';
-import { MdInfoOutline } from 'react-icons/md';
 import { useSelector } from 'react-redux';
 import { toast } from 'react-toastify';
 import {
@@ -8,6 +7,8 @@ import {
 } from '../../lib/features/submission/submissionApi';
 import { RootState } from '../../lib/store';
 import Loading from '../Loading';
+import ExerciseHeader from './ExerciseHeader';
+import ExerciseTable from './ExerciseTable';
 
 type Props = {
   exerciseId: number;
@@ -45,75 +46,8 @@ const ExerciseHistory: React.FC<Props> = ({ exerciseId }) => {
 
   return (
     <div className="w-[1000px]">
-      <div className="mb-8">
-        <p className="pb-2">Exercise</p>
-        <h3 className="text-2xl heading-md">{data.exercise.exerciseName}</h3>
-      </div>
-      <div className="flex items-center justify-between py-5 border-y border-solid border-[#cdcfd5]">
-        <p>Deadline: {data.exercise.deadline}</p>
-        <p>
-          Total tries:
-          {` ${data.submissions.length} / ${data.exercise.maxTries}`}
-        </p>
-        <p>Time limit: {data.exercise.duration}'</p>
-        <p>
-          Minimum score:
-          {` ${data.exercise.minPassingPercentage}%`}
-        </p>
-      </div>
-      <div className="mt-5 mb-6">
-        <table className="tutor-table">
-          <thead>
-            <tr>
-              <th>STT</th>
-              <th>Date</th>
-              <th>Question</th>
-              <th>Correct Answers</th>
-              <th>Incorrect Answers</th>
-              <th>Pending Answers</th>
-              <th>Score</th>
-              <th>Result</th>
-              <th>Details</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.submissions.map((submission: any, index: number) => (
-              <tr key={index}>
-                <td>{submission.tryCount}</td>
-                <td>
-                  <p>{submission.date.split(' ')[0]}</p>
-                  <p>{submission.date.split(' ')[1]}</p>
-                </td>
-                <td>{submission.totalQuestions}</td>
-                <td>{submission.numberOfCorrectAnswers}</td>
-                <td>{submission.numberOfIncorrectAnswers}</td>
-                <td>{submission.numberOfPendingAnswers}</td>
-                <td>{`${submission.gainedPointQuestions} (${submission.percentage}%)`}</td>
-                <td>
-                  <span
-                    className={`${
-                      submission.status === 'PENDING'
-                        ? 'label-warning'
-                        : submission.status === 'PASS'
-                        ? 'label-success'
-                        : 'label-error'
-                    }`}
-                  >
-                    {submission.status}
-                  </span>
-                </td>
-                <td>
-                  <div className="flex justify-center">
-                    <a className="btn btn-medium btn-secondary rounded-md">
-                      <MdInfoOutline className="icon icon-small" />
-                    </a>
-                  </div>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+      <ExerciseHeader />
+      <ExerciseTable submissions={data.submissions} isShowDetail={true} />
       <div className="flex gap-6 w-full justify-start">
         <button
           className="btn btn-medium btn-primary heading-sm rounded-md"
